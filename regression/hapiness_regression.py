05f8ba3db006d1022195de2ae6921481d535b192
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-04-06 08:55:15 -0600

    predicting but non-representative data

--- regression/hapiness_regression.py
+++ regression/hapiness_regression.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 data_root = "https://github.com/ageron/data/raw/main/"
-life_sat_dataset = pr.read_csv(data_root + "/lifesat/lifesat.csv")
+life_sat_dataset = pd.read_csv(data_root + "/lifesat/lifesat.csv")
 X = life_sat_dataset[["GDP per capita (USD)"]].values
 y = life_sat_dataset[["Life satisfaction"]].values
 
@@ -12,4 +12,9 @@ life_sat_dataset.plot(kind="scatter", grid=True, x="GDP per capita (USD)", y="Li
 plt.axis([23_500, 62_500, 4, 9])
 plt.show()
 
+model = LinearRegression()
 
+model.fit(X, y)
+
+X_new = [[37_655.20]]
+print(model.predict(X_new))
