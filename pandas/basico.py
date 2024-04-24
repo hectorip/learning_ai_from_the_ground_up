e91fbeba7c98af8dc8e3a9fbc1d94b780a0357cb
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-04-23 23:58:01 -0600

    Empezando con Pandas

---
+++ pandas/basico.py
@@ -0,0 +1,20 @@
+# Pandas es una biblioteca que permite trabajar con datos de manera
+# eficiente y sencilla.
+# 
+# Una opción más moderna es Polars, pero comparten muchos concepts,
+# así que empezaremos con Pandas.
+
+import pandas as pd
+
+
+# El concepto principal de pandas es el DataFrame: una
+# estructura de datos en forma de tabla con filas y columnas, pero
+# que puede tener más dimensiones.
+
+
+my_data_fram = pd.DataFrame({
+    "a": [1, 2, 3],
+    "b": [4, 5, 6],
+    "c": [7, 8, 9]
+})
+print(my_data_fram)
